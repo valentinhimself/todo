0fdca8a38ae5b690672b325213d6103c0b904e54
Valentin Himself <valentinhimself@gmail.com>
2023-03-08 10:48:18 -0700

    debug edit prompts

--- src/index.js
+++ src/index.js
@@ -31,16 +31,17 @@ DomController.submitBtn.addEventListener('click', () => {
   }
   addToDoToDOM();
   EditPrompts();
-
   console.log(array);
 });
 
 function EditPrompts() {
+  console.log('called Edit Prompts');
   const editPrompt = document.querySelector('.edit-prompt__div ');
   const editIcons = document.querySelectorAll('.edit-icon');
   const titleEdit = document.querySelector('#title-edit');
   const detailsEdit = document.querySelector('#details-edit');
   const dateEdit = document.querySelector('#date-edit');
+  const priorityEditArr = document.querySelector('.edit-priority-btns label');
   const exit = document.querySelector('.exit-edit');
   const editSubmitBtn = document.querySelector('#edit__btn');
   let currentNode;
@@ -51,24 +52,38 @@ function EditPrompts() {
 
   editIcons.forEach((icon) =>
     icon.addEventListener('click', () => {
-      currentNode = icon.parentElement.parentElement;
-      getCurrentNodeToDoTitle(icon);
-      getCurrentNodeIndex(array, currentToDoTitle);
-      setEditValues(currentToDoTitle);
+      // getCurrentNodeToDoTitle(icon);
+      // getCurrentNodeIndex(array, currentToDoTitle);
+      // setEditValues(currentToDoTitle);
+      assignClassCurrentNode(icon);
+      populateEditFields();
       openEditPrompt();
     })
   );
 
-  editSubmitBtn.addEventListener('click', () => {
-    updateToDoArray();
+  editSubmitBtn.addEventListener('click', callOnSubmit);
+
+  function callOnSubmit() {
+    console.log(currentNode);
     updateToDoDOM();
     closeEditPrompt();
-    console.log(array);
-  });
+    resetEdit();
+  }
 
-  function closeEditPrompt() {
-    editPrompt.classList.add('collapse');
-    DomController.bodyContainer.classList.remove('blur');
+  function assignClassCurrentNode(icon) {
+    icon.parentElement.parentElement.classList.add('isBeingEdited');
+    currentNode = document.querySelector('.isBeingEdited');
+  }
+
+  function populateEditFields() {
+    titleEdit.value = currentNode.querySelector('.item-text-title').textContent;
+    selectEditPriorityButton().click();
+  }
+
+  function selectEditPriorityButton() {
+    return document.querySelector(
+      `#${document.querySelector('.isBeingEdited').classList[1]}_lbl-edit`
+    );
   }
 
   function openEditPrompt() {
@@ -76,48 +91,73 @@ function EditPrompts() {
     DomController.bodyContainer.classList.add('blur');
   }
 
-  function getCurrentNodeToDoTitle(icon) {
-    currentToDoTitle =
-      icon.parentElement.parentElement.querySelector(
-        '.item-text-title'
-      ).textContent;
-    return currentToDoTitle;
-  }
-
-  function getCurrentNodeIndex(arr, title) {
-    arrayIndex = arr.findIndex((object) => object.title === title);
-  }
-
-  function setEditValues(title) {
-    titleEdit.textContent = title;
-    detailsEdit.textContent = array[arrayIndex].details;
-    dateEdit.value = array[arrayIndex].dueDate;
-  }
-
-  function updateToDoArray() {
-    // arrayIndex = arr.findIndex((object) => object.title === title);
-
-    array[arrayIndex].title = titleEdit.value;
-    array[arrayIndex].details = detailsEdit.value;
-    array[arrayIndex].dueDate = dateEdit.value;
-    array[arrayIndex].priority = (() => {
-      const checkedValue = document
-        .querySelector('.edit-prompt__div .priority-btns input:checked')
-        .id.slice(0, 3);
-      if (checkedValue === 'low' || checkedValue === 'med') return checkedValue;
-      return 'high';
-    })();
-  }
-
   function updateToDoDOM() {
-    currentNode.querySelector('.item-text-title').textContent =
-      array[arrayIndex].title;
-    // ).textContent = titleEdit.value;
+    if (currentNode) {
+      currentNode.querySelector('.item-text-title').textContent =
+        titleEdit.value;
+      changeItemPriority();
+    }
   }
+
+  function changeItemPriority() {
+    currentNode.classList.remove(currentNode.classList[1]);
+    currentNode.classList.add(
+      document
+        .querySelector('.edit-priority-btns .priority-active')
+        .id.split('_')[0]
+    );
+  }
+
+  function closeEditPrompt() {
+    editPrompt.classList.add('collapse');
+    DomController.bodyContainer.classList.remove('blur');
+  }
+
+  function resetEdit() {
+    if (currentNode) {
+      currentNode.classList.remove('isBeingEdited');
+      currentNode = undefined;
+    }
+  }
+
+  //   function getCurrentNodeToDoTitle(icon) {
+  //     currentToDoTitle =
+  //       icon.parentElement.parentElement.parentElement.querySelector(
+  //         '.item-text-title'
+  //       ).textContent;
+  //     return currentToDoTitle;
+  //   }
+
+  //   function getCurrentNodeIndex(arr, title) {
+  //     arrayIndex = arr.findIndex((object) => object.title === title);
+  //   }
+
+  //   function setEditValues(title) {
+  //     titleEdit.textContent = title;
+  //     detailsEdit.textContent = array[arrayIndex].details;
+  //     dateEdit.value = array[arrayIndex].dueDate;
+  //   }
+
+  //   function updateToDoArray() {
+  //     // arrayIndex = arr.findIndex((object) => object.title === title);
+
+  //     array[arrayIndex].title = titleEdit.value;
+  //     array[arrayIndex].details = detailsEdit.value;
+  //     array[arrayIndex].dueDate = dateEdit.value;
+  //     array[arrayIndex].priority = (() => {
+  //       const checkedValue = document
+  //         .querySelector('.edit-prompt__div .priority-btns input:checked')
+  //         .id.slice(0, 3);
+  //       if (checkedValue === 'low' || checkedValue === 'med') return checkedValue;
+  //       return 'high';
+  //     })();
+  //   }
+
+  //   function updateToDoDOM() {
+  //     console.log(arrayIndex);
+  //     console.log(array[arrayIndex]);
+  //     currentNode.querySelector('.item-text-title').textContent =
+  //       array[arrayIndex].title;
+  //     // ).textContent = titleEdit.value;
+  //   }
 }
-
-//   return { array };
-// }
-
-// const myToDo = () => ToDo();
-// myToDo();
